dc7f7746f3076282654cd8aa564bc781b1f44df2
VoidWolf13 <voidwolf13mail@gmail.com>
2020-03-23 15:21:24 +0300

    Сделал toggle common-blocks/toggle/

---
+++ toxin/common-blocks/toogle/toggle.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('.toggle__item1').click(function() {
+    $('.toggle__item1').toggleClass('toggle__item1--on');
+    $('.toggle__item2').toggleClass('toggle__item2--on');
+  });
+});
